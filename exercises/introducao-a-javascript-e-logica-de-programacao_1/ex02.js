92dc136c70ed2a9532c15db8a41acd5ca96f28d9
Bernardo <benotargiacomo@gmail.com>
2021-06-23 22:22:53 -0300

    Commit bomba exercícios 1-6

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/ex02.js
@@ -0,0 +1,10 @@
+let a = 6;
+let b = 2;
+
+if(a > b){
+  console.log("O valor " + a + " é maior que " + b + "!");
+} else if(b > a) {
+  console.log("O valor " + b + " é maior que " + a + "!");
+} else {
+  console.log("Os valores são iguais!");
+}

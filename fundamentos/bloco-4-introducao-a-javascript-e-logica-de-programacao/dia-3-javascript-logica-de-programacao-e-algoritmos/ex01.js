5447cdf888a6df8efe1bcbd71743f1b220e9a9d2
Bernardo <benotargiacomo@gmail.com>
2021-06-25 19:50:28 -0300

    Exercício 2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/ex01.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/ex01.js
@@ -2,7 +2,7 @@ let n = 3;
 let asteriscoLinha = "";
 
 for (let index = 1; index <= n; index += 1){
-  asteriscoLinha =  asteriscoLinha + "*"
+  asteriscoLinha =  asteriscoLinha + "*";
 }
 
 for (let index = 1; index <= n;index += 1){

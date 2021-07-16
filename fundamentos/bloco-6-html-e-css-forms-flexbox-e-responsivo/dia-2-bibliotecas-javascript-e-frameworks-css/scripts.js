56cf0e0da6d7bc8a12a40d107032728328ac558d
Bernardo <benotargiacomo@gmail.com>
2021-07-15 23:05:12 -0300

    2/3

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-2-bibliotecas-javascript-e-frameworks-css/scripts.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-2-bibliotecas-javascript-e-frameworks-css/scripts.js
@@ -1,5 +1,6 @@
 const estados = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'];
 const estadoSelect = document.getElementById('estado-input');
+const submitButton = document.querySelector('#submit');
 
 for (let i = 0; i < estados.length; i += 1) {
   let options = document.createElement('option');
@@ -8,7 +9,98 @@ for (let i = 0; i < estados.length; i += 1) {
   estadoSelect.append(options);
 }
 
+submitButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  console.log('click');
+})
 
 // function getUserValue(event) {
 //   event.preventDefault();
 // }
+
+window.onload = function() {
+  // document.getElementById('data-input').DatePickerX.init();
+  new window.JustValidate('.js-form', {
+    rules: {
+      nome: {
+        required: true,
+        minLength: 3,
+        maxLength: 40
+      },
+      email: {
+        required: true,
+        email: true,
+        maxLength: 50
+      },
+      cpf: {
+        required: true,
+        maxLength: 11
+      },
+      endereco: {
+        required: true,
+        maxLength: 200
+      },
+      cidade: {
+        required: true,
+        maxLength: 28
+      },
+      estado: {
+        required: true
+      },
+      imovel: {
+        required: true
+      },
+      curriculo: {
+        required: true,
+        maxLength: 1000
+      },
+      cargo: {
+        required: true,
+        maxLength: 40
+      },
+      data: {
+        required: true
+      }
+    },
+    messages: {
+      nome: {
+        required: 'O campo de nome é obrigatório.',
+        maxLength: 'O limite é de 40 caracteres.'
+      },
+      email: {
+        required: 'O campo de email é obrigatório.',
+        email: 'O email digitado não é válido.',
+        maxLength: 'O limite é de 50 caracteres.'
+      },
+      cpf: {
+        required: 'O campo de CPF é obrigatório.',
+        maxLength: 'O limite é de 11 caracteres.'
+      },
+      endereco: {
+        required: 'O campo endereço é obrigatório.',
+        maxLength: 'O limite é de 200 caracteres.'
+      },
+      cidade: {
+        required: 'O campo cidade é obrigatório.',
+        maxLength: 'O limite é de 28 caracteres.'
+      },
+      estado: {
+        required: 'O campo estado é obrigatório.',
+      },
+      imovel: {
+        required: 'A escolha de um item é obrigatória.',
+      },
+      curriculo: {
+        required: 'Este campo é obrigatório.',
+        maxLength: 'O limite é de 1000 caracteres.'
+      },
+      cargo: {
+        required: 'Este campo é obrigatório.',
+        maxLength: 'O limite é de 40 caracteres.'
+      },
+      data: {
+        required: 'Este campo é obrigatório.',
+      }
+    },
+  });
+}
